03787c3b9ad426864069c0a37064549918b8c2b3
sonnybardhan <sonnybardhan@github.io>
2020-06-15 03:43:07 +0530

    add express to serve static files

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-let PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+const express = require('express');
+const app = express();
 const http = require('http');
-
-const httpServer = http.createServer((req, res) => {
-	console.log('we have received a message');
-});
-
 const WebSocket = require('ws');
-// const wss = new WebSocket.Server({port: 8080});
-const wss = new WebSocket.Server({ server: httpServer });
 
-httpServer.listen(PORT, () => console.log('Listening on port 8080'));
+// const httpServer = http.createServer((req, res) => {
+// 	console.log('we have received a message');
+// });
+// const wss = new WebSocket.Server({port: 8080});
+
+const server = http.Server(app);
+app.use(express.static('client'));
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
 	ws.on('message', (data) => {
