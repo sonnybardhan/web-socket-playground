b1f6bf747318451fb3d7723c796fd7ad043b57b4
sonnybardhan <sonnybardhan@github.io>
2020-06-15 03:12:25 +0530

    complete simple chat app

---
+++ client.js
@@ -0,0 +1,38 @@
+const connection = new WebSocket('ws://localhost:8080');
+const button = document.querySelector('#send');
+
+connection.onopen = (event) => {
+	console.log('WebSocket is open now ... ');
+};
+
+connection.onclose = (event) => {
+	console.log('WebSocket is closed now.');
+};
+
+connection.onerror = (error) => {
+	console.error('WebSocket error observed: ', error);
+};
+
+connection.onmessage = (event) => {
+	const chat = document.querySelector('#chat');
+	chat.innerHTML += event.data;
+};
+
+button.addEventListener('click', () => {
+	const name = document.querySelector('#name');
+	const message = document.querySelector('#message');
+
+	const data = `<p><span style="color: ${generateColor()}">[${name.value}]</span>: ${message.value}</p>`;
+	connection.send(data);
+
+	name.value = '';
+	message.value = '';
+});
+
+function randomVals() {
+	return (Math.random() * 256) | 0;
+}
+
+function generateColor() {
+	return `rgb(${randomVals()}, ${randomVals()}, ${randomVals()})`;
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const httpServer = http.createServer((req, res) => {
+	console.log('we have received a message');
+});
+
+const WebSocket = require('ws');
+// const wss = new WebSocket.Server({port: 8080});
+const wss = new WebSocket.Server({ server: httpServer });
+
+httpServer.listen(8080, () => console.log('Listening on port 8080'));
+
+wss.on('connection', (ws) => {
+	ws.on('message', (data) => {
+		wss.clients.forEach((client) => {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(data);
+			}
+		});
+	});
+});
